926b6ea0c41b75a166bbe605bcd67313b62f1772
Michał Mietliński <michal.mietlinski@intive.com>
2017-12-15 03:42:16 +0100

    few first api calls ready

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express();
+const HTTP_PORT = 8099
+
+app.post( '/api/alwaysok', ( req, res ) => {
+    res.json( {message: 'everything is all right'}
+  )
+} )
+app.get( '/api/alwaysok', ( req, res ) => {
+
+    res.json( {message: 'everything is all right'}
+  )
+} )
+app.get( '/api/alwayswrong', ( req, res ) => {
+
+    res.status(500).json({ error: 'message' });
+} )
+app.post( '/api/alwayswrong', ( req, res ) => {
+
+    res.status(500).json({ error: 'message' });
+} )
+
+app.get( '/api/sometimeswrong', ( req, res ) => {
+    let val=Math.random()*100;
+    if(val>75){
+    res.status(500).json({ error: 'message' });
+    }else{
+        res.status(200).json({ info: 'message' });
+    }
+} )
+app.post( '/api/sometimeswrong', ( req, res ) => {
+    let val=Math.random()*100;
+    if(val>75){
+    res.status(500).json({ error: 'message' });
+    }else{
+        res.status(200).json({ info: 'message' });
+    }
+} )
+
+app.post( '/api/choosestatus/:status', ( req, res ) => {
+
+    res.status(req.params.status).json({ info: 'message' });
+} )
+app.get( '/api/choosestatus/:status', ( req, res ) => {
+
+    res.status(req.params.status).json({ info: 'message' });
+} )
+
+
+
+app.listen(HTTP_PORT, () => {
+    console.log( `Server listening on port ${  HTTP_PORT }` )
+    console.log('availlable methods in readme')
+} )
